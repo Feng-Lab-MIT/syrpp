f1aa5e588b42c97398169fd202d67b52f97cad22
Xun Yuan <yuanxun2000@outlook.com>
2024-11-28 03:51:37 -0500

    finish DIR & DIA

--- syr_pump/SyrPump.py
+++ syr_pump/SyrPump.py
@@ -3,22 +3,37 @@ import serial.tools.list_ports
 
 
 class SyrPump:
-    PROMPT = dict(
-        I="infusing",
-        W="withdrawing",
-        S="pumping program stopped",
-        P="pumping program paused",
-        T="pause phase",
-        U="operational trigger wait (user wait)"
-    )
 
-    ALARM = dict(
-        R="pump was reset (power was interrupted)",
-        S="pump motor stalled",
-        T="safe mode communications time out",
-        E="pumping program error",
-        O="pumping program phase is out of range"
-    )
+    PROMPT = {
+        'I': "infusing",
+        'W': "withdrawing",
+        'S': "pumping program stopped",
+        'P': "pumping program paused",
+        'T': "pause phase",
+        'U': "operational trigger wait (user wait)"
+    }
+
+    ALARM = {
+        'R': "pump was reset (power was interrupted)",
+        'S': "pump motor stalled",
+        'T': "safe mode communications time out",
+        'E': "pumping program error",
+        'O': "pumping program phase is out of range"
+    }
+
+    ERROR = {
+        '': "command is not recognized (‘?’ only)",
+        'NA': "command is not currently applicable",
+        'OOR': "command data is out of range",
+        'COM': "invalid communications packet received",
+        'IGN': "command ignored due to a simultaneous new phase start"
+    }
+
+    PUMP_DIRECTION = {
+        'INF': 'infuse',
+        'WDR': 'withdraw',
+        'REV': 'reverse'
+    }
 
     def __init__(
             self,
@@ -41,6 +56,25 @@ class SyrPump:
     def __del__(self):
         self.serial.close()
 
+    def get_diameter(self, address: int) -> float:
+        r = self._cmd(address, 'DIA')
+        return float(r['data'])
+
+    def set_diameter(self, address: int, diameter: float):
+        r = self._cmd(address, 'DIA', self._float(diameter))
+
+    def get_pump_direction(self, address: int) -> str:
+        r = self._cmd(address, 'DIR')
+        assert r['data'] in self.PUMP_DIRECTION.keys(),\
+            f"unknown direction {r['data']}"
+        return self.PUMP_DIRECTION[r['data']]
+
+    def set_pump_direction(self, address: int, direction: str):
+        assert direction in self.PUMP_DIRECTION.values(), \
+            f"unknown direction {direction}"
+        dir = [k for k, v in self.PUMP_DIRECTION.items() if v == direction]
+        r = self._cmd(address, 'DIR', dir[0])
+
     def _raw_cmd(self, cmd: str):
         send = cmd + '\r\n'
         send = send.encode('utf-8')
@@ -53,9 +87,10 @@ class SyrPump:
         receive = receive[1:-1].decode('utf-8')
         return receive
 
-    def _cmd(self, addr: int, cmd: str):
+    def _cmd(self, addr: int, cmd: str, *args: list[str]):
         assert 0 <= addr <= 99, "address out of range"
-        response = self._raw_cmd(str(addr) + cmd)
+        fields = [str(addr), cmd, *args]
+        response = self._raw_cmd(''.join(fields))
         res = dict(
             address=int(response[:2])
         )
@@ -73,12 +108,32 @@ class SyrPump:
         else:
             raise ValueError(f"unknown status: {status}")
         if data:
-            res['data'] = data
+            if data[0] == '?':
+                error = data[1:]
+                assert error in self.ERROR.keys(), f"unknown error: {error}"
+                raise ValueError(f"command error: {self.ERROR[error]}")
+            else:
+                res['data'] = data
         return res
+
+    @staticmethod
+    def _float(f: float) -> str:
+        """
+        Maximum of 4 digits plus 1 decimal point.
+        Maximum of 3 digits to the right of the decimal point.
+        """
+        f = str(round(f, 3))
+        if '.' in f:
+            if len(f) >= 5:
+                f = f[-5:]
+        else:
+            if len(f) >= 4:
+                f = f[-4:]
+        return f
 
 
 if __name__ == '__main__':
     ports = serial.tools.list_ports.comports()
     p = SyrPump('COM7')
-    r = p._cmd(0, 'DIA')
+    p.set_pump_direction(0, 'infuse')
     print(r)
