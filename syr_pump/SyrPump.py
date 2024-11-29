a39cc6b1b6cd076a91a4ad4788ee6c702555540b
Xun Yuan <yuanxun2000@outlook.com>
2024-11-28 23:18:42 -0500

    finish all commands

--- syr_pump/SyrPump.py
+++ syr_pump/SyrPump.py
@@ -1,10 +1,9 @@
 import serial
 import serial.tools.list_ports
-from typing import Any
+from typing import Any, Optional
 
 
 class SyrPump:
-
     PROMPT = {
         'I': "infusing",
         'W': "withdrawing",
@@ -36,14 +35,61 @@ class SyrPump:
         'REV': 'reverse'
     }
 
+    TRIGGER_SETUP = {
+        # falling edge starts or stops the pumping program
+        'FT': ['foot switch', 'foot'],
+        # falling edge stops the pumping program
+        # rising edge starts the pumping program
+        'LE': ['level control', 'level'],
+        # falling edge starts the pumping program
+        'ST': ['start only', 'start']
+    }
+
+    PHASE_FUNCTION = {
+        # rate data functions
+        'RAT': 'rate',
+        'INC': 'increment',
+        'DEC': 'decrement',
+        # non-rate data functions
+        'STP': 'stop',
+        'JMP': 'jump',          # jump to program phase n
+        'LOP': 'loop to',       # loop to previous loop start n times
+        'LPS': 'loop start',    # loop start phase
+        'LPE': 'loop end',      # loop end phase
+        'PAS': 'pause',         # pause pumping for n seconds
+        'IF': 'if',             # if program input TTL pin low, jump to phase n
+        'EVN': 'event trap',    # set event trigger trap to phase n
+        'EVR': 'event reset',   # event trigger reset
+        'BEP': 'beep',
+        'OUT': 'output'         # set programmable output pin (5) 0/1
+    }
+
+    PHASE_FUN_DATA = {
+        'JMP': 'phase',
+        'LOP': 'count',
+        'PAS': 'number',
+        'IF': 'phase',
+        'EVN': 'phase',
+        'OUT': 'ttl'
+    }
+
     VOLUME_UNITS = {
         'U': ['\u03bcl', 'ul', 'microliter'],
-        'M': ['ml', 'milliliter']
+        'M': ['ml', 'milliliter', 'cc']
     }
 
     TIME_UNITS = {
         'M': ['min', 'mn', 'minute'],
         'H': ['h', 'hr', 'hour']
+    }
+
+    DATA_RANGE = {
+        'address': (0, 99),
+        'phase': (1, 41),
+        'number': (0, 99),
+        'count': (1, 99),
+        'ttl': (0, 1),
+        'timeout': (0, 255)
     }
 
     def __init__(
@@ -79,10 +125,36 @@ class SyrPump:
         return int(r['data'])
 
     def set_phase(self, address: int, phase: int):
-        assert 1 <= phase <= 41, f"invalid phase number {phase}"
+        self._check_range(phase, 'phase')
         self._cmd(address, 'PHN', phase)
 
-    def get_pump_rate(self, address: int) -> dict[str, Any]:
+    def get_function(self, address: int) -> dict[str, Any]:
+        """
+        return True if buzzer is on continuously or beeping
+        """
+        r = self._cmd(address, 'FUN')
+        ret = dict()
+        d = r['data'][-2:]
+        if d.isdigit():
+            f = r['data'][:-2]
+            assert f in self.PHASE_FUN_DATA.keys(), f"function {f} should not have data"
+            ret['data'] = int(d)
+        else:
+            f = r['data']
+        assert f in self.PHASE_FUNCTION, f"unknown function {f}"
+        ret['function'] = self._from_dict_key(self.PHASE_FUNCTION, f)
+        return ret
+
+    def set_function(self, address: int, function: str, data: Optional[int] = None):
+        f = self._from_dict_value(self.PHASE_FUNCTION, function)
+        args = list()
+        args.append(f)
+        if data is not None:
+            assert f in self.PHASE_FUN_DATA.keys(), f"function {f} should not have data"
+            args.append(data)
+        self._cmd(address, 'BUZ', *args)
+
+    def get_rate(self, address: int) -> dict[str, Any]:
         r = self._cmd(address, 'RAT')
         return dict(
             value=float(r['data'][:-2]),
@@ -90,18 +162,92 @@ class SyrPump:
             time_unit=self._from_dict_key(self.TIME_UNITS, r['data'][-1])
         )
 
-    def set_pump_rate(self, address: int, rate: float, volume_unit: str, time_unit: str):
+    def set_rate(self, address: int, rate: float, volume_unit: str, time_unit: str):
         vu = self._from_dict_value(self.VOLUME_UNITS, volume_unit)
         tu = self._from_dict_value(self.TIME_UNITS, time_unit)
         self._cmd(address, 'RAT', self._float(rate), vu, tu)
 
-    def get_pump_direction(self, address: int) -> str:
+    def get_direction(self, address: int) -> str:
         r = self._cmd(address, 'DIR')
         return self._from_dict_key(self.PUMP_DIRECTION, r['data'])
 
-    def set_pump_direction(self, address: int, direction: str):
+    def set_direction(self, address: int, direction: str):
         d = self._from_dict_value(self.PUMP_DIRECTION, direction)
         self._cmd(address, 'DIR', d)
+
+    def get_com_mode(self, address: int) -> dict[str, Any]:
+        r = self._cmd(address, 'SAF')
+        timeout = int(r['data'])
+        if timeout == 0:
+            return {'mode': 'basic'}
+        else:
+            return {'mode': 'safe', 'timeout': timeout}
+
+    def set_com_mode(self, address: int, mode: str, timeout: int = None):
+        args = list()
+        if mode == 'basic':
+            assert timeout is None, f"no timeout for basic communication mode"
+            args.append(0)
+        elif mode == 'safe':
+            assert timeout is not None, f"timeout should be provided for safe communication mode"
+            args.append(timeout)
+        self._cmd(address, 'SAF', *args)
+
+    def get_alarm(self, address: int) -> bool:
+        """
+        return True if alarm buzzer mode is enabled
+        when alarms are enabled, the buzzer will be sounded as follows:
+        condition                                   | buzzer action
+        ----------------------------------------------------------------
+        pumping program ended                       | continuous beeping
+        pumping program paused for start trigger    | continuous beeping
+        alarm condition, such as pump motor stalled | steady alarm
+        """
+        r = self._cmd(address, 'AL')
+        return bool(int(r['data']))
+
+    def set_alarm(self, address: int, buzzer: bool):
+        self._cmd(address, 'AL', int(buzzer))
+
+    def get_power_fail(self, address: int) -> bool:
+        r = self._cmd(address, 'PF')
+        return bool(int(r['data']))
+
+    def set_power_fail(self, address: int, power_fail: bool):
+        self._cmd(address, 'PF', int(power_fail))
+
+    def get_trigger(self, address: int) -> str:
+        """
+        return True if buzzer is on continuously or beeping
+        """
+        r = self._cmd(address, 'TRG')
+        return self._from_dict_key(self.TRIGGER_SETUP, r['data'])
+
+    def set_trigger(self, address: int, trigger: str):
+        self._cmd(address, 'TRG', self._from_dict_value(self.TRIGGER_SETUP, trigger))
+
+    def get_key_beep(self, address: int) -> bool:
+        r = self._cmd(address, 'BP')
+        return bool(int(r['data']))
+
+    def set_key_beep(self, address: int, key_beep: bool):
+        self._cmd(address, 'BP', int(key_beep))
+
+    def set_ttl_output(self, address: int, level: int, pin: int = 5):
+        """
+        sets TTL level on user definable output pin on the TTL I/O connector
+        """
+        assert pin == 5, f"only pin 5 is supported"
+        self._check_range(level, 'ttl')
+        self._cmd(address, 'OUT', pin, level)
+
+    def get_ttl_input(self, address: int, level: int, pin: int):
+        """
+        queries TTL level of pin on TTL I/O connector
+        """
+        assert pin in [2, 3, 4, 6], f"pin {pin} not supported"
+        r = self._cmd(address, 'IN', pin)
+        return r['data']
 
     def get_buzzer(self, address: int) -> bool:
         """
@@ -110,11 +256,12 @@ class SyrPump:
         r = self._cmd(address, 'BUZ')
         return bool(int(r['data']))
 
-    def set_buzzer(self, address: int, buzzer: bool, ntime=0):
+    def set_buzzer(self, address: int, buzzer: bool, n_time: int = 0):
         """
-        if ntime == 0, buzzer beeps continuously
+        if n_time == 0, buzzer beeps continuously
         """
-        self._cmd(address, 'BUZ', int(buzzer), ntime)
+        # TODO: check why buzzer cannot be set true
+        self._cmd(address, 'BUZ', int(buzzer), n_time)
 
     def start_program(self, address: int):
         self._cmd(address, 'RUN')
@@ -157,7 +304,10 @@ class SyrPump:
         return receive
 
     def _cmd(self, addr: int, cmd: str, *args):
-        assert 0 <= addr <= 99, "address out of range"
+        """
+        return the code only because this is a private method
+        """
+        self._check_range(addr, 'address')
         args = [str(a) if not isinstance(a, str) else a for a in args]
         fields = [str(addr), cmd, *args]
         response = self._raw_cmd(''.join(fields))
@@ -171,15 +321,16 @@ class SyrPump:
         elif status == 'A':
             assert response[3] == '?', "'?' missing for alarm response"
             alarm_type = response[4]
-            res['alarm'] = self._from_dict_key(self.ALARM, alarm_type)
+            self._from_dict_key(self.ALARM, alarm_type)
+            res['alarm'] = alarm_type
             data = response[5:]
         else:
             raise ValueError(f"unknown status: {status}")
         if data:
             if data[0] == '?':
                 error = data[1:]
-                msg = self._from_dict_key(self.ERROR, error)
-                raise ValueError(f"command error: {msg}")
+                self._from_dict_key(self.ERROR, error)
+                res['error'] = error
             else:
                 res['data'] = data
         return res
@@ -187,8 +338,8 @@ class SyrPump:
     @staticmethod
     def _float(f: float) -> str:
         """
-        Maximum of 4 digits plus 1 decimal point.
-        Maximum of 3 digits to the right of the decimal point.
+        maximum of 4 digits plus 1 decimal point
+        maximum of 3 digits to the right of the decimal point
         """
         f = str(round(f, 3))
         if '.' in f:
@@ -214,10 +365,16 @@ class SyrPump:
         assert len(k) == 1
         return k[0]
 
+    @staticmethod
+    def _check_range(v: int, type: str):
+        assert type in SyrPump.DATA_RANGE.keys(), f"unknown data type {type}"
+        r = SyrPump.DATA_RANGE[type]
+        assert r[0] <= v <= r[1], f"{v} out of range for {type} data"
+
 
 if __name__ == '__main__':
     ports = serial.tools.list_ports.comports()
     p = SyrPump('COM7')
-    #p.set_buzzer(0, True)
-    r = p.get_pump_rate(0)
+    p.set_function(0, 'rate')
+    r = p.get_function(0)
     print(r)
